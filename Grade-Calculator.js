337b8e0d26fea2c4790c84cf2974c5f4c6f797c2
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2024-11-29 10:54:51 +0300

    not end

--- Grade-Calculator.js
+++ Grade-Calculator.js
@@ -10,9 +10,12 @@
 
 function calculateGrade(c,python,js,php ){
      let average= (c+python+js+php)/4;
+     console.log(`Your score: ${average}`)
      return average;
 }
 
 // const grade = [a = 4, aMinus=3.75, bPlus=3.5, b =3.25, bMinus=3 , cPlus = 2.75 , c=2.5, cMinus=2.25, d=2];
 
-console.log(calculateGrade(3,10,5,2));
+calculateGrade(3,10,5,2);
+
+
