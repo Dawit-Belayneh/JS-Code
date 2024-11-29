ad13ea4b196fa623801a8c21eeb5e2c0015accec
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2024-11-29 10:31:21 +0300

    change the number to grade

--- Grade-Calculator.js
+++ Grade-Calculator.js
@@ -1,6 +1,18 @@
 // first create variable for the subjects
-
- 
+    // the greade a b c d 
+    //create a subject and assign the greade
+    // assign to what the value of grade
+    // how did you get the student
+    //than calculate the average
 // than add the point this subject 
 //calculate the average point
 // output the average score
+
+function calculateGrade(c,python,js,php ){
+     let average= (c+python+js+php)/4;
+     return average;
+}
+
+// const grade = [a = 4, aMinus=3.75, bPlus=3.5, b =3.25, bMinus=3 , cPlus = 2.75 , c=2.5, cMinus=2.25, d=2];
+
+console.log(calculateGrade(3,10,5,2));
